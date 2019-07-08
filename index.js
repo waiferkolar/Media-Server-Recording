e25cc4556593fd8a1c1cee8ef18404e555a746d2
waiferkolar <72coder@gmail.com>
2019-07-08 18:52:04 +0630

    Serving HTML File

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let http = require('http');
 let url = require('url');
 let qs = require('querystring');
+let fs = require('fs');
 require('dotenv').config()
 
 let responder = (req, res, param) => {
@@ -8,13 +9,35 @@ let responder = (req, res, param) => {
     res.end(param);
 }
 
+let myFileReader = (filepath, res) => {
+    fs.access(filepath, fs.F_OK, (err) => {
+        if (err) {
+            res.writeHead(404, { "Content-type": "text/html" });
+            res.end("<h1>File not found</h1>");
+        } else {
+
+            fs.readFile(filepath, (err, data) => {
+                if (err) throw err;
+                res.writeHead(200, { "Content-type": "text/html" });
+                res.end(data);
+            });
+        }
+    })
+}
+
 let routes = {
     "GET": {
         "/": (req, res) => {
-            responder(req, res, `<h1>Get Method => / route</h1>`);
+            let filepath = __dirname + '/index.html';
+            myFileReader(filepath, res);
         },
-        "/home": (req, res) => {
-            responder(req, res, `<h1>Get Method => /home route with param of ${params.query.name} and ${params.query.age}</h1>`);
+        "/index.html": (req, res) => {
+            let filepath = __dirname + "/index.html";
+            myFileReader(filepath, res);
+        },
+        "/about.html": (req, res) => {
+            let filepath = __dirname + "/about.html";
+            myFileReader(filepath, res);
         }
     },
     "POST": {
@@ -25,6 +48,10 @@ let routes = {
             let body = '';
             req.on('data', data => {
                 body += data;
+                if (body.length > 1024) {
+                    res.writeHead(403, { 'Content-type': 'text/html' });
+                    res.end("<h1>File Size over 1mb!</h1>");
+                }
             });
             req.on('end', () => {
                 let query = qs.parse(body);
