c0319f6d6d451929f7c0d6f0d6aecce5be562fb4
waiferkolar <72coder@gmail.com>
2019-07-08 17:51:11 +0630

    Getting Post urlencoded value

--- index.js
+++ index.js
@@ -1,46 +1,54 @@
 let http = require('http');
 let url = require('url');
+let qs = require('querystring');
 require('dotenv').config()
+
+let responder = (req, res, param) => {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(param);
+}
 
 let routes = {
     "GET": {
-        "/": (req, res, params) => {
-            res.writeHead(200, { "Content-type": "text/html" });
-            res.end("<h1>Get Method => / route</h1>");
+        "/": (req, res) => {
+            responder(req, res, `<h1>Get Method => / route</h1>`);
         },
-        "/home": (req, res, params) => {
-            res.writeHead(200, { "Content-type": "text/html" });
-            res.end(`<h1>Get Method => /home route with param of ${params.query.name} and ${params.query.age}</h1>`);
+        "/home": (req, res) => {
+            responder(req, res, `<h1>Get Method => /home route with param of ${params.query.name} and ${params.query.age}</h1>`);
         }
     },
     "POST": {
-        "/": (req, res, params) => {
-            res.writeHead(200, { "Content-type": "text/html" });
-            res.end("<h1>Post Method => / route</h1>");
+        "/": (req, res) => {
+            responder(req, res, `<h1>Post Method => / route</h1>`);
         },
-        "/about": (req, res, params) => {
-            res.writeHead(200, { "Content-type": "text/html" });
-            res.end("<h1>Post Method => /about route</h1>");
+        "/api/login": (req, res) => {
+            let body = '';
+            req.on('data', data => {
+                body += data;
+            });
+            req.on('end', () => {
+                let query = qs.parse(body);
+                console.log("Email ", query.email, " Password ", query.password);
+                res.end();
+            })
         },
     },
-    "NA": (req, res, params) => {
-        res.writeHead(404);
-        res.end("<h1>No page for that route!</h1>");
+    "NA": (req, res) => {
+        responder(req, res, `<h1>No page for that route!</h1>`);
     }
 }
 let start = (req, res) => {
     let reqMethod = req.method;
     let params = url.parse(req.url, true);
-    let name = params.query.name;
-    let age = params.query.age;
+    // let name = params.query.name;
+    // let age = params.query.age;
 
-    console.log("Name ", name, " Age ", age);
 
     let resolveRoute = routes[reqMethod][params.pathname];
     if (resolveRoute != null && resolveRoute != undefined) {
-        resolveRoute(req, res, params);
+        resolveRoute(req, res);
     } else {
-        routes["NA"](req, res, params);
+        routes["NA"](req, res);
     }
 }
 
