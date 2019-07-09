c7c248c81d1c355d8e15388069ba2a528c347b1c
waiferkolar <72coder@gmail.com>
2019-07-09 18:52:58 +0630

    More modi

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-let libby = require('./bmlibby/index');
+require('dotenv').config();
+let express = require('express'),
+    app = express(),
+    path = require('path');
 
+app.use(express.static(path.join('assets')));
 
-libby.encode('123')
-    .then(encoded => libby.compare('124', encoded))
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+app.get('/loading', (req, res) => {
+    res.sendFile(__dirname + "/loading.html");
+})
+app.get('/index', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + "/about.html");
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running at Port ${process.env.PORT}`);
+});
