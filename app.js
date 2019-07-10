d5fd6beaefc144963240c65351e6d9f2b0363c88
waiferkolar <72coder@gmail.com>
2019-07-10 18:23:44 +0630

    Socket Middle

--- app.js
+++ app.js
@@ -13,11 +13,31 @@ app.use(express.static(path.join(__dirname, 'assets')));
 app.get('/', (req, res) => {
     res.render('index');
 })
+/** 
+    #sending to All Users
+    io.emit('key','val');
 
+    #send message only to sender-client
+    socket.emit('message', 'check this');
+
+    #send to all listeners except sender
+    socket.broadcast.emit("chat-data",username + " : " + data);
+
+    #send message to certain user
+    io.sockets.connected[clients[1]].emit("chat-data",username + " : " + data);
+
+    #send to specific socket-id (specific user)
+    socket.broadcast.to(socketid).emit("chat-data",username + " : " + data);
+    io.sockets.connected[socket.id].emit('login-success', true)
+
+    // Socket , io emit 
+    // Socket Room 
+    // namespace
+*/
 io.sockets.on('connection', (socket) => {
     socket.on("login", data => {
         socket.username = data;
-        io.sockets.connected[socket.id].emit('login-success', true)
+        socket.broadcast.to(socket.id).emit('login-success',true);
     });
     socket.on('msg', data => {
         io.emit('income-msg', socket.username + " : " + data);
